666b0cb672a78f12ce8891401029dbf94d99a1ab
Ali Kaafarani <ali@kvikshaug.no>
2016-10-04 15:15:11 +0200

    chore: redirect script output to pid 1 stdout
    This ensures that output is visible in Docker logs for pid 1.

--- letsencrypt/bin/entrypoint.sh
+++ letsencrypt/bin/entrypoint.sh
@@ -2,7 +2,9 @@
 
 set -e
 
+STDOUT=/proc/1/fd/1
+
 # Wait for HAproxy to start before updating certificates on startup.
-(sleep 60; update-certs.sh) &
+(sleep 60; update-certs.sh > ${STDOUT}) &
 
 exec "$@"
